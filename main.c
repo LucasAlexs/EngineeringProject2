6daad7b954ef5670a94448a52fac43e71b04b295
Lucas Alexsandro <lucas.leal.santos@itec.ufpa.br>
2023-03-21 19:25:13 -0300

    Projeto de Engenharia II

--- main.c
+++ main.c
@@ -5,13 +5,17 @@ int main()
 {
     char str[]= "NOME DO ALUNO";
 
-    //nome aqui
     char name1[]= "LUCAS ALEXSANDRO LEAL DOS SANTOS:";
+    char name2[]= "SAULO MATTHEUS RIBEIRO DE OLIVEIRA:";
+    char name3[]= "JOSE HENRIQUE BARBOSA PENA:";
+    char name4[]= "GABRIEL DE JESUS DOS SANTOS COSTA:";
 
     char str2[]= "MATRICULA";
 
-    //matricula aqui
     char * matri1= "(202207040035)";
+    char * matri2= "(202206840041)";
+    char * matri3= "(202206840036)";
+    char * matri4= "(202206840044)";
 
     char divisoria[]= "------------------------------------";
     char divisoria2[]= " --------------";
@@ -21,6 +25,10 @@ int main()
     printf("%s \t", divisoria);
     printf("%s \n", divisoria2);
     printf("%s \t %s \n", name1, matri1);
+    printf("%s \t %s \n", name2, matri2);
+    printf("%s \t\t %s \n", name3, matri3);
+    printf("%s \t %s \n", name4, matri4);
+
 
     return 0;
 }
