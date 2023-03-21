1f0c398019195880a138e014c818cf2c816329e8
Saulo Oliveira <81710047+S4ulo-Th3us@users.noreply.github.com>
2023-03-21 20:04:49 -0300

    MAIN-MAN
    Eu adicionei meu nome e número de matrícula

--- GRUPO FRIDAY.c
+++ GRUPO FRIDAY.c
@@ -6,14 +6,16 @@ int main()
     char str[]= "NOME DO ALUNO";
 
     //nome aqui
-    char name1[]= "LUCAS ALEXSANDRO LEAL DOS SANTOS:";
-    char name2[]= "GABRIEL DE JESUS DOS SANTOS COSTA:";
+    char name1[]= "LUCAS ALEXSANDRO LEAL DOS SANTOS;";
+    char name2[]= "GABRIEL DE JESUS DOS SANTOS COSTA;";
+    char name3[]= "SAULO MATTHEUS RIBEIRO DE OLIVEIRA;";
 
     char str2[]= "MATRICULA";
 
     //matricula aqui
     char * matri1= "(202207040035)";
     char * matri2= "(202206840044)";
+    char * matri3= "(202206840041)";
 
     char divisoria[]= "------------------------------------";
     char divisoria2[]= " --------------";
@@ -24,5 +26,7 @@ int main()
     printf("%s \n", divisoria2);
     printf("%s \t %s \n", name1, matri1);
     printf("%s \t %s \n", name2, matri2);
+    printf("%s \t %s \n", name3, matri3);
+    
     return 0;
 }
