6c94f697cbda8562166e70d650fdb4228923881a
Gabriel8Kbps <128176412+Gabriel8Kbps@users.noreply.github.com>
2023-06-19 01:50:32 +0000

    Update matrizes.h

--- src/matrizes/matrizes.h
+++ src/matrizes/matrizes.h
@@ -1,6 +1,9 @@
 #ifndef MATRIZES_H
 #define MATRIZES_H
 
+/**### typedef struct Complex
+* - É uma estrutura de dados que permite representar números complexos em linguagem c 
+*/
 typedef struct Complex
 {
 
@@ -15,22 +18,22 @@ struct Complex somanc(struct Complex a,struct Complex b);
 struct Complex multiplicacao(struct Complex x,struct Complex y);
 
 struct Complex **soma(struct Complex **matrix1,struct Complex **matrix2, int linhas, int colunas);
-int teste_soma();
+void teste_soma();
 
 struct Complex **subtracao(struct Complex **matrix1,struct Complex **matrix2, int linhas, int colunas);
-int teste_subtracao();
+void teste_subtracao();
 
 struct Complex **transposta(struct Complex **matrix1, int linhas, int colunas);
-int teste_transposta();
+void teste_transposta();
 
 struct Complex **conjugado(struct Complex **matrix1, int linhas, int colunas);
-int teste_conjugada();
+void teste_conjugada();
 
 struct Complex **hermitiano(struct Complex **matrix1, int linhas, int colunas);
-int teste_hermitiana();
+void teste_hermitiana();
 
 struct Complex produto_escalar(struct Complex *vet1,struct Complex *vet2, int neu, int nev);
-int teste_produto_escalar();
+void teste_produto_escalar();
 
 struct Complex **produto_matricial(struct Complex **matrix1,struct Complex **matrix2, int linhas, int colunas);
 
