e57349b382b30f2b5b97cc1e6bd4e51726ce4930
Gabriel8Kbps <128176412+Gabriel8Kbps@users.noreply.github.com>
2023-07-02 02:28:14 +0000

    Update pds_telecom.c

--- src/matrizes/pds_telecom.c
+++ src/matrizes/pds_telecom.c
@@ -9,35 +9,30 @@ void main()
 
 void tx_data_read()
 {
-char texto_str[1024];
+    char texto_str[128];
     FILE *pont_arq, *ptarq;
-    pont_arq = fopen("src/matrizes/arquivo.txt", "a");
+    printf("Digite o Nome do Arquivo: ");
+    gets(texto_str);
+    pont_arq = fopen(texto_str, "a");
     ptarq = fopen ("src/matrizes/binario.bin", "wb");
     fclose(pont_arq);
 
-    printf("Arquivo criado!\n");
-
-    if (pont_arq == NULL){
-    printf("ERRO! O arquivo não foi aberto!\n");
+    if (pont_arq == NULL)
+    {
+        printf("ERRO! O arquivo não foi aberto!\n");
     }
-    else{
-    printf("O arquivo foi aberto com sucesso!\n");
+    else
+    {
+        printf("O arquivo foi aberto com sucesso!\n");
     }
 
-    pont_arq = fopen("src/matrizes/arquivo.txt", "r");
+    pont_arq = fopen(texto_str, "r");
+
     printf("---------- Conteudo do Arquivo: ----------\n\n");
-    while(fgets(texto_str, 1024, pont_arq) != NULL){
-    printf("%s", texto_str);
-    fclose(pont_arq);
+    while(fgets(texto_str, 128, pont_arq) != NULL)
+    {
+        printf("%s", texto_str);
+        fclose(pont_arq);
     }
     printf("\n------------------------------------------\n\n");
-
-    if (ptarq!=NULL)
-    printf ("\n\n Arquivo binario.bin foi aberto com sucesso \n");
-    else {
-    printf ("\n\n Erro: O arquivo binario.bin não foi aberto \n");
-    system ("pause");
-    exit (0);}
-
-    fwrite (texto_str, sizeof (char), 1024, ptarq);
 }
