459439c6603bdddf47eb6be6400c7e4b40556c24
henrylog10 <104786990+henrylog10@users.noreply.github.com>
2023-07-02 16:18:54 -0300

    Update main.c
    tentativa das duas primeiras funções

--- src/main.c
+++ src/main.c
@@ -4,7 +4,26 @@
 
 int main()
 {
-    tx_data_read();
-    
+    const char *filename = "arquivo.txt";
+
+    // Leitura do arquivo e conversão para sequência de inteiros
+    int size;
+    int *data = tx_data_read(filename, &size);
+    if (data == NULL) {
+        return 1;
+    }
+
+    // Impressão dos dados lidos
+    printf("Sequência de inteiros: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+
+    // Escrita dos dados convertidos de volta para o arquivo
+    rx_data_write("arquivo_output.txt", data, size);
+
+    // Liberação de memória
+    free(data);
     return 0;
 }
