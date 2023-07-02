324d9ea8b44653f0cd29acbeda6bef35ca75a303
Gabriel8Kbps <128176412+Gabriel8Kbps@users.noreply.github.com>
2023-07-02 01:04:18 +0000

    Update mimo.c

--- src/matrizes/mimo.c
+++ src/matrizes/mimo.c
@@ -6,31 +6,31 @@ void main()
 {
     char texto_str[1024];
     FILE *pont_arq, *ptarq;
-    pont_arq = fopen("arquivo.txt", "a");
-    ptarq = fopen ("binario.bin", "wb");
+    pont_arq = fopen("src/matrizes/arquivo.txt", "a");
+    ptarq = fopen ("src/matrizes/binario.bin", "wb");
     fclose(pont_arq);
 
     printf("Arquivo criado!\n");
 
     if (pont_arq == NULL){
-    printf("ERRO! O arquivo n„o foi aberto!\n");
+    printf("ERRO! O arquivo n√£o foi aberto!\n");
     }
     else{
     printf("O arquivo foi aberto com sucesso!\n");
     }
 
-    pont_arq = fopen("arquivo.txt", "r");
+    pont_arq = fopen("src/matrizes/arquivo.txt", "r");
     printf("---------- Conteudo do Arquivo: ----------\n\n");
     while(fgets(texto_str, 1024, pont_arq) != NULL){
     printf("%s", texto_str);
     fclose(pont_arq);
     }
-    printf("\n\n------------------------------------------\n\n");
+    printf("\n------------------------------------------\n\n");
 
     if (ptarq!=NULL)
     printf ("\n\n Arquivo binario.bin foi aberto com sucesso \n");
     else {
-    printf ("\n\n Erro: O arquivo binario.bin n„o foi aberto \n");
+    printf ("\n\n Erro: O arquivo binario.bin n√£o foi aberto \n");
     system ("pause");
     exit (0);}
 
