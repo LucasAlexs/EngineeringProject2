69885010900658cca4c76f12b0459795c30a31de
henrylog10 <104786990+henrylog10@users.noreply.github.com>
2023-07-02 19:45:27 -0300

    Update pds_telecom.h
    adicionando a função tx_layer_mapper ao cabeçalho

--- src/matrizes/pds_telecom.h
+++ src/matrizes/pds_telecom.h
@@ -5,6 +5,8 @@ int hexToDecimal(char hex);
 
 int *tx_data_read(const char *filename, int *size);
 
+int * tx_layer_mapper(struct Complex * symbol,int size);
+
 void rx_data_write(const char *filename, const int *data, int size);
 
 
