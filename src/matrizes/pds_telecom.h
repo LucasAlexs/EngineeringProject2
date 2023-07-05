2568b57245a547e734b26e3b8d5913fb6e01301e
henrylog10 <104786990+henrylog10@users.noreply.github.com>
2023-07-05 10:21:40 -0300

    Update pds_telecom.h

--- src/matrizes/pds_telecom.h
+++ src/matrizes/pds_telecom.h
@@ -1,12 +1,13 @@
 #ifndef PDS_TELECOM_H
 #define PDS_TELECOM_H
 
-int hexToDecimal(char hex);
+void print_binario(unsigned char byte, int* vetor, long* index);
 
-int *tx_data_read(const char *filename, int *size);
+int *tx_data_read(const char *texto_str, long *tamanho_retornado);
 
-struct Complex * tx_qam_mapper(int* indice, int size) ;
+struct Complex * tx_qam_mapper(int* indice, int size);
 
 int * rx_qam_demapper(struct Complex * symbol,int size);
 
 #endif
+
