b1db4428cdc01e1663cea6525f18adde76231bbf
Gabriel8Kbps <128176412+Gabriel8Kbps@users.noreply.github.com>
2023-07-07 23:11:56 +0000

    Update pds_telecom.h

--- src/matrizes/pds_telecom.h
+++ src/matrizes/pds_telecom.h
@@ -3,11 +3,11 @@
 
 void print_binario(unsigned char byte, int* vetor, long* index);
 
-int *tx_data_read(const char *texto_str, long *tamanho_retornado);
+int* tx_data_read(const char *texto_str, long *tamanho_retornado);
 
-struct Complex * tx_qam_mapper(int* indice, int size);
+struct Complex *tx_qam_mapper(int* indice, int size);
 
-int * rx_qam_demapper(struct Complex * symbol,int size);
+int *rx_qam_demapper(struct Complex * symbol,int size);
 
 struct Complex **channel_gen(int Nr,struct Complex **H, int Nt);
 
