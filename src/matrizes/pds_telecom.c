28f1c169f5576d28a93cb5cbdc39246f9984922e
Gabriel8Kbps <128176412+Gabriel8Kbps@users.noreply.github.com>
2023-07-08 15:58:25 -0300

    Update pds_telecom.c

--- src/matrizes/pds_telecom.c
+++ src/matrizes/pds_telecom.c
@@ -1,121 +1,137 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include "matrizes.h"
 #include "pds_telecom.h"
+#include <time.h>
 
-int main()
-{
-   //tx_data_read();
+int main(){
 
-   int Nr = 4, Nt = 4 ,size = 32, Nstreams,Nqam = 4;
-    struct Complex *s,*s_mapped, *o;
+    FILE *arquivo_txt;
+    arquivo_txt = fopen("src/matrizes/arquivo.txt", "rb");
 
-   if (Nr < Nt) {
-        Nstreams = Nr;
-    } else {
-        Nstreams = Nt;
-    }
+    fseek(arquivo_txt, 0, SEEK_END);
+    long int q_bits = ftell(arquivo_txt);
+    fseek(arquivo_txt, 0, SEEK_SET);
 
-    int *vector;
-
-    vector = (int *)malloc(size * sizeof(int));
-
-    if (vector == NULL) {
-        printf("Erro ao alocar memória para o vetor.\n");
-        return 1;
-    }
-
-    for (int i = 0; i < size; i++) {
-        vector[i] = i % Nqam;
-    }
-
-    printf("Elementos do vetor Etapa1:\n");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", vector[i]);
-    }
-    printf("\n");
-
-
-    s_mapped = (struct Complex *)malloc(Nstreams * sizeof(struct Complex ));
-    o = (struct Complex *)malloc(Nt * sizeof(struct Complex ));
-
-    s = tx_qam_mapper(vector,size);
-
-    for (int a = 0; a < size; a+= Nstreams)
-    {
-        s_mapped = tx_layer_mapper(a,s,s_mapped,Nstreams);
-
-        o = rx_layer_demapper(a,s_mapped,o,Nstreams);
-    }
-
-    vector = rx_qam_demapper(o,size);
-
-    printf("Elementos do vetor Etapa2:\n");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", vector[i]);
-    }
-    printf("\n");
-
-    free(vector);
-    free(s_mapped);
-
+    int *vetor = tx_data_read(arquivo_txt, q_bits);
+    fclose(arquivo_txt);
     return 0;
+    //Quantidade de bits
+
+    //rx_data_write(vetor, q_bits, "arquivo.bin");
+
+
+
+//    int Nr = 4,Nt = 4;
+//    struct Complex **H,**J,**K;
+//    double rmax=0,rmin=0;
+//    srand(time(NULL));
+//
+//    H = (struct Complex **)malloc(Nr * sizeof(struct Complex *));
+//    J = (struct Complex **)malloc(Nr * sizeof(struct Complex *));
+//
+//    for (int i = 0; i < Nr; i++)
+//    {
+//        H[i] = (struct Complex *)malloc(Nt * sizeof(struct Complex));
+//        J[i] = (struct Complex *)malloc(Nt * sizeof(struct Complex));
+//    }
+//
+//    H = channel_gen(Nr,H, Nt);
+//
+//    printf("matriz H:\n");
+//
+//    for(int i=0; i<Nr; i++){
+//        for(int j=0; j<Nt; j++){
+//            printf("%.2f + %.2fj\t", H[i][j].real, H[i][j].img);
+//        }
+//        printf("\n");
+//    }
+//
+//    J = channel_gen(Nr,J, Nt);
+//
+//    printf("matriz J:\n");
+//
+//    for(int i=0; i<Nr; i++){
+//        for(int j=0; j<Nt; j++){
+//            printf("%.2f + %.2fj\t", J[i][j].real, J[i][j].img);
+//        }
+//        printf("\n");
+//    }
+//
+//    K = channel_transmission(rmax,rmin,J,H,Nr,Nt);
+//
+//    printf("matriz K:\n");
+//
+//    for(int i=0; i<Nr; i++){
+//        for(int j=0; j<Nt; j++){
+//            printf("%.2f + %.2fj\t", K[i][j].real, K[i][j].img);
+//        }
+//        printf("\n");
+//    }
+//
+//    for (int i = 0; i < Nr; i++){
+//            free(H[i]);
+//            free(J[i]);
+//        }
+//        free(H);
+//        free(J);
+//
+//
+//        return 0;
 }
 
-void print_binario(unsigned char byte, int* vetor, long int* index) {
-    for (int i = 6; i >= 0; i -= 2) {
-        int num = ((byte >> i) & 1) + ((byte >> (i + 1)) & 1) * 2;
-        vetor[(*index)++] = num;
+int * tx_data_read(FILE *texto_str, long int tamanho_retornado) { //long*
+
+    // Alocacão de memoria
+    int *vet_int = (int *)malloc(tamanho_retornado * 4 * sizeof(int));
+    if (vet_int == NULL) {
+        printf("Erro na alocação de memória\n");
+        fclose(texto_str);
+        return (int *)1;
     }
+
+    // Converte os bytes em inteiros de 2 bits
+    for (int i = 0; i < tamanho_retornado; i++)
+        {
+            char byte;
+            fread(&byte, sizeof(byte), 1, texto_str);
+
+            for (int j = 0; j <= 7; j=j+2)
+            {
+                int bit = (byte >> j) & 3;
+                vet_int[(i*4) + (j/2)]= bit;
+            }
+
+    }
+
+    return vet_int;
 }
 
-int* tx_data_read(const char* texto_str, long* tamanho_retornado) {
-    FILE* file = fopen(texto_str, "rb");
-    if (file == NULL) {
-        printf("Erro! O arquivo não pode ser aberto.\n");
-        return NULL;
+void rx_data_write(int* entrada_vet_int, long int q_bits) {
+
+    FILE* binario = fopen("src/matrizes/arquivo.bin", "wb");
+
+    if (binario == NULL) {
+        printf("Erro! Arquivo.bin nao pode ser aberto!\n");
+        return;
+
+    } else {
+        printf("Arquivo.bin gerado com sucesso.\n");
     }
 
-    fseek(file, 0, SEEK_END);
-    long tamanho = ftell(file);
-    rewind(file);
-    unsigned char* buffer = (unsigned char*)malloc(tamanho);
-    if (buffer == NULL) {
-        printf("Erro! Memória não pode ser alocada.\n");
-        fclose(file);
-        return NULL;
-    }
-  
-      size_t leitura_bytes = fread(buffer, 1, tamanho, file);
-    if (leitura_bytes != tamanho) {
-        printf("Erro ao ler o arquivo.\n");
-        free(buffer);
-        fclose(file);
-        return NULL;
+    for (int i = 0; i < q_bits; i++) {
+
+        unsigned char byte = 0;
+        for (int j = 0; j < 4; j++)
+        {
+            unsigned int bit = entrada_vet_int[(i * 4) + j];
+            byte |= (bit << (2 * j));
+        }
+        fwrite(&byte, sizeof(byte), 1, binario);
     }
 
-    int* vetor = (int*)malloc((tamanho * 4) * sizeof(int)); // Cada byte gera 4 dígitos de 2 bits
-    if (vetor == NULL) {
-        printf("Erro! Memória não pode ser alocada.\n");
-        free(buffer);
-        fclose(file);
-        return NULL;
-    }
-
-    long int index = 0;
-    for (long i = 0; i < tamanho; i++) {
-        print_binario(buffer[i - 1], vetor, &index);
-    }
-
-    free(buffer);
-    fclose(file);
-
-    *tamanho_retornado = tamanho * 4;
-    return vetor;
+    fclose(binario);
 }
-
-
 
 struct Complex *tx_qam_mapper(int* indice, int size) {
     struct Complex *symbol;
@@ -135,7 +151,7 @@ struct Complex *tx_qam_mapper(int* indice, int size) {
             symbol[i].real = 1;
             symbol[i].img = 1;
         }
-        else if (indice[i] == 3){
+        else if (indice[i] == 2){
             symbol[i].real = 1;
             symbol[i].img = -1;
         }
@@ -173,8 +189,8 @@ struct Complex *rx_layer_demapper(int a, struct Complex *s_mapped,struct Complex
     // Loop para percorrer os símbolos de entrada
     for (int i = 0; i < Nstreams; i++) {
         // Mapeia o símbolo QAM para a stream correspondente
-        s[(a) + i ].real = s_mapped[i].real;
-        s[(a) + i ].img = s_mapped[i].img ;
+        s[(a * Nstreams) + i].real = s_mapped[i].real;
+        s[(a * Nstreams) + i].img = s_mapped[i].img ;
     }
     return s;
 }
@@ -184,8 +200,8 @@ struct Complex *tx_layer_mapper(int a, struct Complex *s,struct Complex *s_mappe
     // Loop para percorrer os símbolos de entrada
     for (int i = 0; i < Nstreams; i++) {
         // Mapeia o símbolo QAM para a stream correspondente
-        s_mapped[i].real = s[(a ) + i ].real;
-        s_mapped[i].img = s[(a) + i ].img;
+        s_mapped[i].real = s[a * Nstreams + i].real;
+        s_mapped[i].img = s[a * Nstreams + i].img;
     }
     return s_mapped;
 }
@@ -193,7 +209,7 @@ struct Complex *tx_layer_mapper(int a, struct Complex *s,struct Complex *s_mappe
 struct Complex **channel_gen(int Nr,struct Complex **H, int Nt) {
 
     // Gera os valores aleatórios para a matriz H
-    srand(time(NULL));  // Inicializa a semente do gerador de números aleatórios
+      // Inicializa a semente do gerador de números aleatórios
 
     for (int i = 0; i < Nr; i++) {
         for (int j = 0; j < Nt; j++) {
