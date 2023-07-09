af5232b55461e780f7c2552dc129da01623a6c01
Gabriel8Kbps <128176412+Gabriel8Kbps@users.noreply.github.com>
2023-07-09 19:28:50 +0000

    Update pds_telecom.h

--- src/matrizes/pds_telecom.h
+++ src/matrizes/pds_telecom.h
@@ -1,18 +1,24 @@
 #ifndef PDS_TELECOM_H
 #define PDS_TELECOM_H
 
-void print_binario(unsigned char byte, int* vetor, long* index);
+int teste_mapper();
+void teste_svd();
+void teste_rw();
 
-int * tx_data_read(FILE *entrada_arquivo, long int q_bytes);
+void rx_data_write(int* entrada_vet_int, long int tamanho);
+
+int * tx_data_read(FILE* entrada_arquivo, long int q_bytes);
 
 struct Complex *tx_qam_mapper(int* indice, int size);
 
 int *rx_qam_demapper(struct Complex * symbol,int size);
 
+struct Complex *rx_layer_demapper(int a, struct Complex *s_mapped,struct Complex *s, int Nstreams);
+
+struct Complex *tx_layer_mapper(int a, struct Complex *s,struct Complex *s_mapped, int Nstreams);
+
 struct Complex **channel_gen(int Nr,struct Complex **H, int Nt);
 
 struct Complex **channel_transmission(double rmax, double rmin, struct Complex **mtx_cod, struct Complex **H, int Nr, int Nt);
-
-void rx_data_write(int* entrada_vet_int, long int tamanho);
 
 #endif
