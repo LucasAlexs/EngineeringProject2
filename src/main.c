d9bce0eaef90e42be714c0d61c09737d367593fe
Gabriel8Kbps <128176412+Gabriel8Kbps@users.noreply.github.com>
2023-07-09 20:59:32 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,22 +5,5 @@
 
 int main()
 {
-    const char* arquivo = "arquivo.txt";
-    long tamanho;
-    int* vetor = tx_data_read(arquivo, &tamanho);
-    if (vetor == NULL) {
-        printf("Erro ao ler o arquivo.\n");
-        return 1;
-    }
-
-    printf("Valores retornados:\n");
-    for (long i = 0; i < tamanho; i++) {
-        printf("%d ", vetor[i]);
-    }
-
-    printf("\n");
-
-    free(vetor);
-
     return 0;
 }
