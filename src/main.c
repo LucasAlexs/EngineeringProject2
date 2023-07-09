36de3f53bda1ea15151f482110a4b40d15d5fec6
henrylog10 <104786990+henrylog10@users.noreply.github.com>
2023-07-09 02:56:33 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,22 +5,6 @@
 
 int main()
 {
-    const char* arquivo = "arquivo.txt";
-    long tamanho;
-    int* vetor = tx_data_read(arquivo, &tamanho);
-    if (vetor == NULL) {
-        printf("Erro ao ler o arquivo.\n");
-        return 1;
-    }
-
-    printf("Valores retornados:\n");
-    for (long i = 0; i < tamanho; i++) {
-        printf("%d ", vetor[i]);
-    }
-
-    printf("\n");
-
-    free(vetor);
-
+    teste_todos();
     return 0;
 }
