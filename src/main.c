b4338c70b45d93ef56aeada8016f8b6744510979
Gabriel8Kbps <128176412+Gabriel8Kbps@users.noreply.github.com>
2023-07-09 02:16:59 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "matrizes/matrizes.h"
-#include "matrizes/pds_telecom.h"
-
-int main()
-{
-    const char* arquivo = "arquivo.txt";
-    long tamanho;
-    int* vetor = tx_data_read(arquivo, &tamanho);
-    if (vetor == NULL) {
-        printf("Erro ao ler o arquivo.\n");
-        return 1;
-    }
-
-    printf("Valores retornados:\n");
-    for (long i = 0; i < tamanho; i++) {
-        printf("%d ", vetor[i]);
-    }
-
-    printf("\n");
-
-    free(vetor);
-
-    return 0;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include "matrizes/matrizes.h"
+//#include "matrizes/pds_telecom.h"
+//
+//int main()
+//{
+//    const char* arquivo = "arquivo.txt";
+//    long tamanho;
+//    int* vetor = tx_data_read(arquivo, &tamanho);
+//    if (vetor == NULL) {
+//        printf("Erro ao ler o arquivo.\n");
+//        return 1;
+//    }
+//
+//    printf("Valores retornados:\n");
+//    for (long i = 0; i < tamanho; i++) {
+//        printf("%d ", vetor[i]);
+//    }
+//
+//    printf("\n");
+//
+//    free(vetor);
+//
+//    return 0;
+//}
